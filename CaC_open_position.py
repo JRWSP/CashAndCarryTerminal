3b4b8957aa0f371273473e6cc97bea6c019f9902
Jirawat S <57433713+JRWSP@users.noreply.github.com>
2024-11-25 20:02:30 +0100

    public 1.0

--- CaC_open_position.py
+++ CaC_open_position.py
@@ -1,18 +1,21 @@
-my_position = [
-    {
-        'Exchange': "Kucoin",
-        'Symbol': 'BTC/USD:BTC-241227',
-        'FutureShortPrice': 56726.3,
-        'SpotBuy': {   'AvgPrice': [55607], 
-                    'Amount': [104.86221]
-                    }
-    },
-    {
-        'Exchange': "Bybit",
-        'Symbol': 'BTC/USD:BTC-241227',
-        'FutureShortPrice': 60156.62,
-        'SpotBuy': {   'AvgPrice': [57335.99], 
-                    'Amount': [105.78490155]
-                    }
-    }
+my_position = []
+"""
+[
+{
+    'Exchange': "Kucoin",
+    'Symbol': 'BTC/USD:BTC-241227',
+    'FutureShortPrice': 56726.3,
+    'SpotBuy': {   'AvgPrice': [55607], 
+                'Amount': [104.86221]
+                }
+},
+{
+    'Exchange': "Bybit",
+    'Symbol': 'BTC/USD:BTC-241227',
+    'FutureShortPrice': 60156.62,
+    'SpotBuy': {   'AvgPrice': [57335.99], 
+                'Amount': [105.78490155]
+                }
+}
 ]
+"""
