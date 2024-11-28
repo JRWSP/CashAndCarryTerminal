659268d13f3f52d4d87a57fa066dcaf80dfae0b0
Jirawat S <57433713+JRWSP@users.noreply.github.com>
2024-11-28 18:15:31 +0100

    Add CreateDataFrame in fetch function.

--- CaC_functions_fetch.py
+++ CaC_functions_fetch.py
@@ -3,8 +3,7 @@ import numpy as np
 import pandas as pd
 import ccxt
 
-
-def DeliveryTimeFromTicker(ticker:str):
+def DeliveryTimeFromTicker(ticker:str) -> str:
     yymmdd = ticker[-6:]
     return datetime.strptime(yymmdd,'%y%m%d')  #Date code, e.g. yymmdd.
 
@@ -14,8 +13,39 @@ def FindDaysUntilExpire(APY:float, Spread:float) -> float:
 def FindAPY(Spread:float, Days:float) -> float:
     return ((1+Spread/100)**Days - 1)*100
 
+def ComputeAPYwithFee(df:pd.DataFrame, df_fee:pd.DataFrame) -> pd.DataFrame:
+    """    
+    Return a DataFrame with computed spread and APY after fees.
+    APY is computed using remaining days till expiration.
+    Args:
+        df (pd.DataFrame): main table
+        df_fee (pd.DataFrame): table containing information about CEX's fee.
+
+    Returns:
+        pd.DataFrame: main table with fee-adjusted APY.
+    """
+    merge_fee = pd.merge(df, df_fee, on="Exchange")
+    N = FindDaysUntilExpire(merge_fee.APY, merge_fee.Spread)
+    merge_fee['Spr.-f'] = merge_fee['Spread'] - merge_fee['TotalFee']
+    #merge_fee['SpreadTaker'] = merge_fee['Spread'] - merge_fee['Taker']
+    merge_fee['APY-f'] = FindAPY((merge_fee["Spr.-f"]), N)
+    #merge_fee['APY_Taker'] = FindAPY((merge_fee.SpreadTaker), N)
+    return merge_fee.loc[:, ['Symbol', 'Exchange', 'Spr.-f','APY-f']]
+
+def CreateDataFrame(DeliveryTime:str, timestamp:datetime, 
+                    markPrice:float, indexPrice:float, 
+                    ex_name:str, symbol:str) -> pd.DataFrame:
+        RemainingDay = (DeliveryTime - timestamp).total_seconds()/60/60/24
+        Days = 365/RemainingDay
+        Spread = (markPrice/indexPrice - 1.0)*100
+        APY = FindAPY(Spread, Days)
+        return pd.DataFrame({'Exchange':[ex_name], 'Symbol': [symbol], 
+                               'Spread': Spread, 'APY': APY, 
+                               'Spot': indexPrice, 'Future': markPrice})
+
 def FetchTickersBybit(Tickers: list[str]):
     exchange = ccxt.bybit()
+    ex_name = 'Bybit'
     market = exchange.load_markets()
     timestamp = datetime.fromtimestamp(exchange.milliseconds()/1000)
     FetchedTickers = exchange.fetch_tickers(Tickers)
@@ -25,20 +55,13 @@ def FetchTickersBybit(Tickers: list[str]):
         markPrice = float(ticker['info']['markPrice'])
         indexPrice = float(ticker['info']['indexPrice'])
         DeliveryTime = datetime.fromtimestamp(int(ticker['info']['deliveryTime'])/1000)
-        RemainingDay = (DeliveryTime - timestamp).total_seconds()/60/60/24
-        Days = 365/RemainingDay
-        Spread = (markPrice/indexPrice - 1.0)*100
-        APY = FindAPY(Spread, Days)
-        df = pd.DataFrame({'Exchange':['Bybit'], 'Symbol': [symbol], 
-                               'Spread': Spread, 
-                               'APY': APY, 
-                               'Spot': indexPrice, 
-                               'Future': markPrice})
+        df = CreateDataFrame(DeliveryTime,timestamp,markPrice,indexPrice,ex_name,symbol)
         SpreadData = pd.concat([SpreadData, df], ignore_index=True)
     return SpreadData
 
 def FetchTickersDeribit(Tickers: list[str]):
     exchange = ccxt.deribit()
+    ex_name = 'Deribit'
     market = exchange.load_markets()
     SpreadData = pd.DataFrame()
     """
@@ -69,20 +92,13 @@ def FetchTickersDeribit(Tickers: list[str]):
             symbol = ticker['symbol']
             markPrice = float(ticker['info']['mark_price'])
             DeliveryTime = datetime.strptime(ticker['symbol'][-6:],'%y%m%d')  #Date code, e.g. yymmdd.
-            RemainingDay = (DeliveryTime - timestamp).total_seconds()/60/60/24
-            Days = 365/RemainingDay
-            Spread = (markPrice/SpotPrice - 1.0)*100
-            APY = FindAPY(Spread, Days)
-            df = pd.DataFrame({'Exchange':[exchange.name], 'Symbol': [symbol], 
-                                'Spread': Spread, 
-                                'APY': APY, 
-                                'Spot': SpotPrice, 
-                                'Future': markPrice})
+            df = CreateDataFrame(DeliveryTime,timestamp,markPrice,SpotPrice,ex_name,symbol)
             SpreadData = pd.concat([SpreadData, df], ignore_index=True) 
     return SpreadData
 
 def FetchTickersKucoin(Tickers: list[str]):
     exchange = ccxt.kucoinfutures()
+    ex_name = "Kucoin"
     market = exchange.load_markets()
     timestamp = datetime.fromtimestamp(exchange.milliseconds()/1000)
     FetchedTickers = exchange.fetch_tickers(Tickers)
@@ -92,20 +108,13 @@ def FetchTickersKucoin(Tickers: list[str]):
         markPrice = float(ticker['info']['markPrice'])
         indexPrice = float(ticker['info']['indexPrice'])
         DeliveryTime = datetime.fromtimestamp(ticker['info']['settleDate']/1000)
-        RemainingDay = (DeliveryTime - timestamp).total_seconds()/60/60/24
-        Days = 365/RemainingDay
-        Spread = (markPrice/indexPrice - 1.0)*100
-        APY = FindAPY(Spread, Days)
-        df = pd.DataFrame({'Exchange':['Kucoin'], 'Symbol': [symbol], 
-                               'Spread': Spread, 
-                               'APY': APY, 
-                               'Spot': indexPrice, 
-                               'Future': markPrice})
+        df = CreateDataFrame(DeliveryTime,timestamp,markPrice,indexPrice,ex_name,symbol)
         SpreadData = pd.concat([SpreadData, df], ignore_index=True)
     return SpreadData
 
 def FetchTickersOKX(Tickers: list[str]):
     exchange = ccxt.okx()
+    ex_name = "OKX"
     market = exchange.load_markets()
     #Get Spot prices.
     for ticker in Tickers:
@@ -123,20 +132,11 @@ def FetchTickersOKX(Tickers: list[str]):
         markPrice = float(ticker['ask'])
         indexPrice = float(FetchedSpot[symbol[:3]+"/USDT"]['ask'])
         DeliveryTime = DeliveryTimeFromTicker(symbol)
-        RemainingDay = (DeliveryTime - timestamp).total_seconds()/60/60/24
-        Days = 365/RemainingDay
-        Spread = (markPrice/indexPrice - 1.0)*100
-        APY = FindAPY(Spread, Days)
-        df = pd.DataFrame({'Exchange':['OKX'], 'Symbol': [symbol], 
-                               'Spread': Spread, 
-                               'APY': APY, 
-                               'Spot': indexPrice, 
-                               'Future': markPrice})
+        df = CreateDataFrame(DeliveryTime,timestamp,markPrice,indexPrice,ex_name,symbol)
         SpreadData = pd.concat([SpreadData, df], ignore_index=True)
     return SpreadData
 
 def FetchTickersBinance(Tickers: list[str]):
-
     def GroupedSpotSymbol(Tickers):
         grouped_tickers = []
         for Ticker in Tickers:
@@ -146,10 +146,10 @@ def FetchTickersBinance(Tickers: list[str]):
         return grouped_tickers
     
     exchange = ccxt.binancecoinm()
+    ex_name = "Binance"
     market = exchange.load_markets()
     grouped_tickers = GroupedSpotSymbol(Tickers)
     grouped_tickers = [ticker+"/USDT" for ticker in grouped_tickers]
-    SpotPriceDict = {}
     FetchSpot = ccxt.binance().fetch_tickers(grouped_tickers)
     FetchedTickers = exchange.fetch_tickers(Tickers)
     SpreadData = pd.DataFrame()
@@ -161,33 +161,6 @@ def FetchTickersBinance(Tickers: list[str]):
         indexPrice = float(Spot['last'])
         DeliveryTime = market[symbol]['expiry']
         DeliveryTime = datetime.fromtimestamp(int(DeliveryTime)/1000)
-        RemainingDay = (DeliveryTime - timestamp).total_seconds()/60/60/24
-        Days = 365/RemainingDay
-        Spread = (markPrice/indexPrice - 1.0)*100
-        APY = FindAPY(Spread, Days)
-        df = pd.DataFrame({'Exchange':["Binance"], 'Symbol': [symbol], 
-                               'Spread': Spread, 
-                               'APY': APY, 
-                               'Spot': indexPrice, 
-                               'Future': markPrice})
+        df = CreateDataFrame(DeliveryTime,timestamp,markPrice,indexPrice,ex_name,symbol)
         SpreadData = pd.concat([SpreadData, df], ignore_index=True)
     return SpreadData
-
-def ComputeAPYwithFee(df:pd.DataFrame, df_fee:pd.DataFrame) -> pd.DataFrame:
-    """    
-    Return a dataframe with computed spread and APY after fees.
-    APY is computed using remaining days till expiration.
-    Args:
-        df (pd.DataFrame): main table
-        df_fee (pd.DataFrame): table containing information about CEX's fee.
-
-    Returns:
-        pd.DataFrame: main table with fee-adjusted APY.
-    """
-    merge_fee = pd.merge(df, df_fee, on="Exchange")
-    N = FindDaysUntilExpire(merge_fee.APY, merge_fee.Spread)
-    merge_fee['Spr.-f'] = merge_fee['Spread'] - merge_fee['TotalFee']
-    #merge_fee['SpreadTaker'] = merge_fee['Spread'] - merge_fee['Taker']
-    merge_fee['APY-f'] = FindAPY((merge_fee["Spr.-f"]), N)
-    #merge_fee['APY_Taker'] = FindAPY((merge_fee.SpreadTaker), N)
-    return merge_fee.loc[:, ['Symbol', 'Exchange', 'Spr.-f','APY-f']]
